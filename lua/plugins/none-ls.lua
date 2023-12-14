151a16113db1dab3c03b3a6a8ab2da7f098b5abb
Uzair Aftab <uzaaft@outlook.com>
2023-12-14 20:49:58 +0100

    fix(none-ls): Swap out `jose-elias-alvarez/null-ls` with `nvimtools/none-ls`

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,8 +8,8 @@ return {
     -- local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
