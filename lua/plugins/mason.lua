550a3b6caba6595d4276ca20fe3f551a315701f1
Micah Halter <micah@mehalter.com>
2024-01-19 15:57:33 -0500

    chore(mason): make it easier to modify `list_insert_unique` call

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,8 +9,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed
-        -- "lua_ls",
+        opts.ensure_installed,
+        "lua_ls"
         -- add more arguments for adding more language servers
       )
     end,
@@ -22,9 +22,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed
-        -- "prettier",
-        -- "stylua",
+        opts.ensure_installed,
+        "prettier",
+        "stylua"
         -- add more arguments for adding more null-ls sources
       )
     end,
@@ -35,8 +35,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed
-        -- "python",
+        opts.ensure_installed,
+        "python"
         -- add more arguments for adding more debuggers
       )
     end,
