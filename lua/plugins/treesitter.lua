2813f57ab9e0a627b20a27d8b87ce2b82e345c43
Micah Halter <micah@mehalter.com>
2024-01-19 15:56:43 -0500

    chore(treesitter): make it easier to modify `list_insert_unique` call

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,8 +6,9 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(
-      opts.ensure_installed
-      -- "lua",
+      opts.ensure_installed,
+      "lua",
+      "vim"
       -- add more arguments for adding more treesitter parsers
     )
   end,
