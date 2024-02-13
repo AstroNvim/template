75d2b26b6ef588a9134c9a7f24f14edc7b278ee7
Micah Halter <micah@mehalter.com>
2024-02-13 15:39:40 -0500

    refactor: move `maplocalleader` to AstroNvim `opts`

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ lazy.setup({
     import = "astronvim.plugins",
     opts = { -- AstroNvim options must be set with the `import` key
       mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
       icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
       pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
     },
