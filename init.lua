4ab91873cc4448c3682fb31c49b76322ff82abf9
Micah Halter <micah@mehalter.com>
2024-02-15 10:56:31 -0500

    chore: remove unnecessary global from template

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or vim.loop.fs_stat(lazypath)) then
-  vim.g.astronvim_first_install = true -- lets AstroNvim know that this is an initial installation
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
