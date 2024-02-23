b2b495ff0d02180e3b89f48d61c1bc05740dd9c5
Micah Halter <micah@mehalter.com>
2024-02-23 15:53:28 -0500

    doc: clarify lua language server setup

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -2,7 +2,8 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (lua_ls) as this provides autocomplete and documentation while editing
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
 
 ---@type LazySpec
 return {
--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -2,7 +2,8 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
--- NOTE: We highly recommend setting up the Lua Language Server (lua_ls) as this provides autocomplete and documentation while editing
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
 
 ---@type LazySpec
 return {
