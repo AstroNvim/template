9e3be30b92ed4c210c5fa27883ad805cccdef712
Micah Halter <micah@mehalter.com>
2024-03-05 14:13:36 -0500

    fix!: `diagnostics` configuration moved to AstroCore

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -15,8 +15,14 @@ return {
       large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
       autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
+      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {
+      virtual_text = true,
+      underline = true,
     },
     -- vim options can be configured here
     options = {
--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -14,14 +14,8 @@ return {
     features = {
       autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
       inlay_hints = false, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
     },
     -- customize lsp formatting options
     formatting = {
