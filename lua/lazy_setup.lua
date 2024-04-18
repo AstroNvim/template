5bb68fae07ba9ecf317c7b9a062760c91e05250b
Micah Halter <micah@mehalter.com>
2024-04-18 10:36:52 -0400

    feat(lazy_setup): add new auto update pinned plugins option description

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -8,6 +8,7 @@ require("lazy").setup({
       maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
       icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
       pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+      auto_update_pinned = false, -- Automatically run a second `:Lazy update` after AstroNvim updates to update pinned plugins
     },
   },
   { import = "community" },
