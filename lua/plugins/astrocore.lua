16d0839d84b2770a811416ae9b722ed6246e5c78
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-06-06 05:03:51 +0200

    chore: match the default large buffer size from AstroNvim

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -12,7 +12,7 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
       autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
       diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
