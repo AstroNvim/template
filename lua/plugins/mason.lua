7af067e0ffef9975e8e8054d8455e7662dfb1fa0
Micah Halter <micah@mehalter.com>
2024-06-07 09:26:07 -0400

    refactor(plugins): `ensure_installed` tables now extend by default

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,35 +8,32 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "lua_ls",
         -- add more arguments for adding more language servers
-      })
-    end,
+      },
+    },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "stylua",
         -- add more arguments for adding more null-ls sources
-      })
-    end,
+      },
+    },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "python",
         -- add more arguments for adding more debuggers
-      })
-    end,
+      },
+    },
   },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,12 +5,11 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  opts = {
+    ensure_installed = {
       "lua",
       "vim",
       -- add more arguments for adding more treesitter parsers
-    })
-  end,
+    },
+  },
 }
