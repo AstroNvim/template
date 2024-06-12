7049a195c87d579565d2f65460d5666c805f1c81
Arismendy Evangelista <ArismendyEZ115@hotmail.com>
2024-06-12 19:48:51 -0400

    Readding neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,12 +1,13 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+    {"nvim-lua/plenary.nvim" , lazy = true },        -- required
+    { "sindrets/diffview.nvim", lazy = true },        -- optional - Diff integration
     --
     -- -- Only one of these is needed, not both.
     -- "nvim-telescope/telescope.nvim", -- optional
     -- "ibhagwan/fzf-lua",              -- optional
   },
-  config = true
+  config = true,
+  cmd = "Neogit"
 }
