f3df84dec12e6aa1a3cfebfb100986950dbb037e
Micah Halter <micah@mehalter.com>
2024-06-25 11:59:04 -0400

    fix(lazy_setup): `astrotheme` is default colorscheme

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrodark", "habamax" } },
+  install = { colorscheme = { "astrotheme", "habamax" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
