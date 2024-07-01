b3f629a30eda77c88fa0112e7e3eba07cc06c042
Micah Halter <micah@mehalter.com>
2024-07-01 04:13:04 -0400

    fix(astrolsp): remove old config option

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -12,7 +12,6 @@ return {
   opts = {
     -- Configuration table of features provided by AstroLSP
     features = {
-      autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
       inlay_hints = false, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
