99cb668bc0df1728afc8c1a4ad5744df9a8f4acd
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-10-27 10:21:25 +0100

    Add missing word in polish.lua

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,7 +1,7 @@
 if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
+-- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
 -- Set up custom filetypes
